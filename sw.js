574d7e9c8d9f4e560c86232de6bd8caf949ce9bc
prokiki <44457658+prokiki@users.noreply.github.com>
2025-08-09 20:36:34 +0800

    test

---
+++ sw.js
@@ -0,0 +1,28 @@
+
+// Very small service worker for offline caching
+const CACHE_NAME = 'family-reward-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
